1cea52a375a7b870149b26877ab8352080ededba
odi <odi@panter.ch>
2023-12-23 22:13:46 +0100

    Add NextAuth authentication providers
    This commit adds the necessary code to configure authentication providers for NextAuth. It includes GoogleProvider and CredentialsProvider. The code also includes a mock data section for testing purposes.

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,62 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const { GOOGLE_CLIENT_ID = "", GOOGLE_CLIENT_SECRET = "" } = process.env;
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+    }),
+
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: "Credentials",
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "username" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        // You need to provide your own logic here that takes the credentials
+        // submitted and returns either a object representing a user or value
+        // that is false/null if the credentials are invalid.
+        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+        // You can also use the `req` object to obtain additional parameters
+        // (i.e., the request IP address)
+        // const res = await fetch("/your/endpoint", {
+        //   method: "POST",
+        //   body: JSON.stringify(credentials),
+        //   headers: { "Content-Type": "application/json" },
+        // });
+        // const user = await res.json();
+
+        // ====> mock data start
+
+        const res = { ok: true };
+        const user = {
+          id: "123456789",
+          email: "urbanswisstionary@gmail.com",
+          image:
+            "https://lh3.googleusercontent.com/a/ACg8ocISFmQW488HHTfszh7mrhLWZ-u8wmTfgSw6f32pVOBm=s96-c",
+          name: "urban swisstionary",
+        };
+        // ====> mock data end
+
+        // If no error and we have user data, return it
+        if (res.ok && user) return user;
+
+        // Return null if user data could not be retrieved
+        return null;
+      },
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
