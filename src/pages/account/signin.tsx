5cf86df64da79ad9f89c6290bb4e9ea9e46e04e7
odi <odi@panter.ch>
2024-01-08 21:30:33 +0100

    Refactor SigninPage component

--- src/pages/account/signin.tsx
+++ src/pages/account/signin.tsx
@@ -1,47 +1,36 @@
-import JoiLink from "@mui/joy/Link";
-import Link from "next/link";
 import Typography from "@mui/joy/Typography";
 import Stack from "@mui/joy/Stack";
 
 import GoogleIcon from "@/ui/icons/GoogleIcon";
-import { useSession, signIn } from "next-auth/react";
-import SigninForm from "@/features/Auth/signin";
+import { signIn } from "next-auth/react";
+
 import AuthLayout from "@/features/Auth/layout";
 import { NextPage } from "next";
-import Divider from "@/ui/Divider";
+
 import Button from "@/ui/Button";
 import RecaptchaProvider from "@/providers/Recaptcha";
 
-const SigninPage: NextPage = () => {
-  const { data: session } = useSession();
-
-  console.log(session);
-
-  return (
-    <RecaptchaProvider>
-      <AuthLayout>
-        <Stack gap={1}>
-          <Typography level="h3">Sign in</Typography>
-          <Typography level="body-sm">
-            already have an account?{" "}
-            <JoiLink component={Link} href="/account/signup" level="title-sm">
-              Sign up!
-            </JoiLink>
-          </Typography>
-        </Stack>
-        <Stack gap={4}>
-          <SigninForm />
-        </Stack>
-        <Divider>or</Divider>
+const SigninPage: NextPage = () => (
+  <RecaptchaProvider>
+    <AuthLayout>
+      <Stack gap={4}>
+        <Typography level="h3" textAlign={"center"} textTransform={"uppercase"}>
+          Sign in
+        </Typography>
         <Button
           startDecorator={<GoogleIcon />}
-          onClick={() => signIn("google")}
+          onClick={() =>
+            signIn("google", {
+              redirect: true,
+              callbackUrl: "/account/profile",
+            })
+          }
         >
           Continue with Google
         </Button>
-      </AuthLayout>
-    </RecaptchaProvider>
-  );
-};
+      </Stack>
+    </AuthLayout>
+  </RecaptchaProvider>
+);
 
 export default SigninPage;
