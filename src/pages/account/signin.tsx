0bb3f8e83d2b7db062ed16b1dfa65cff306998c1
odi <odi@panter.ch>
2024-02-07 08:37:30 +0100

    feat (sign-in page): remove reCaptcha

--- src/pages/account/signin.tsx
+++ src/pages/account/signin.tsx
@@ -5,50 +5,26 @@ import { signIn } from "next-auth/react"
 import LayoutWithImage from "@/features/layout/layoutWithImage"
 import type { NextPage } from "next"
 import Button from "@/ui/Button"
-import RecaptchaProvider from "@/providers/Recaptcha"
-import { useCallback, useState } from "react"
-import { ReCaptcha } from "next-recaptcha-v3"
-import { useRouter } from "next/router"
 
 const SigninPage: NextPage = () => {
-  const router = useRouter()
-  const [token, setToken] = useState("")
-  const [disableButton, setDisableButton] = useState(false)
-
-  const onClick = useCallback(async () => {
-    const res = await fetch("/api/recaptcha", { method: "POST", body: token })
-    if (!res.ok) router.reload()
-    else {
-      setDisableButton(true)
-      signIn("google", {
-        redirect: true,
-        callbackUrl: "/",
-      })
-    }
-  }, [router, token])
+  const onClick = () => {
+    signIn("google", {
+      redirect: true,
+      callbackUrl: "/",
+    })
+  }
 
   return (
-    <RecaptchaProvider>
-      <LayoutWithImage>
-        <ReCaptcha onValidate={setToken} action="sign_in" />
-        <Stack gap={4}>
-          <Typography
-            level="h3"
-            textAlign={"center"}
-            textTransform={"uppercase"}
-          >
-            Sign in
-          </Typography>
-          <Button
-            startDecorator={<GoogleIcon />}
-            onClick={onClick}
-            disabled={disableButton}
-          >
-            Continue with Google
-          </Button>
-        </Stack>
-      </LayoutWithImage>
-    </RecaptchaProvider>
+    <LayoutWithImage>
+      <Stack gap={4}>
+        <Typography level="h3" textAlign={"center"} textTransform={"uppercase"}>
+          Sign in
+        </Typography>
+        <Button startDecorator={<GoogleIcon />} onClick={onClick}>
+          Continue with Google
+        </Button>
+      </Stack>
+    </LayoutWithImage>
   )
 }
 export default SigninPage
