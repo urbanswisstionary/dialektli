b40ee3c15ad9de7b267b4939e00d5626b74b7c29
odi <odi@panter.ch>
2024-02-22 10:30:46 +0100

    feat newTermButton can now be disabled

--- src/ui/NewTermButton.tsx
+++ src/ui/NewTermButton.tsx
@@ -1,14 +1,22 @@
 import type { FC } from "react"
-import Link from "next/link"
+import NextLink from "next/link"
 import IconButton, { IconButtonProps } from "@mui/joy/IconButton"
 import AddIcon from "@mui/icons-material/Add"
-
-const NewTermButton: FC<Pick<IconButtonProps, "size">> = ({ size = "sm" }) => (
-  <Link href={"/term/new"} passHref>
-    <IconButton title="New Term" variant="outlined" color="neutral" size={size}>
+import JoyLink from "@mui/joy/Link"
+const NewTermButton: FC<
+  Pick<IconButtonProps, "size"> & { disabled?: boolean }
+> = ({ size = "sm", disabled }) => (
+  <JoyLink component={NextLink} href={"/term/new"} passHref disabled={disabled}>
+    <IconButton
+      title="New Term"
+      variant="outlined"
+      color="neutral"
+      size={size}
+      disabled={disabled}
+    >
       <AddIcon />
     </IconButton>
-  </Link>
+  </JoyLink>
 )
 
 export default NewTermButton
