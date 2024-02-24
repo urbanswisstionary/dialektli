bc6578057388b140d5bf95c1d131cfc21ede211b
odi <odi@panter.ch>
2024-02-24 15:31:44 +0100

    Refactor footer component layout

--- src/features/layout/footer.tsx
+++ src/features/layout/footer.tsx
@@ -2,17 +2,29 @@ import type { FC } from "react"
 import Box from "@mui/joy/Box"
 import Typography from "@mui/joy/Typography"
 import SelectLanguage from "@/ui/Autocomplete/SelectLanguage"
+import Grid from "@mui/joy/Grid"
 
 const Footer: FC = () => (
   <Box
     component="footer"
-    pt={4}
-    sx={{ width: "100%", maxWidth: "1024px", margin: "0 auto" }}
+    sx={{
+      width: "100%",
+      maxWidth: "1024px",
+      margin: "0 auto",
+      mt: "auto",
+      py: 2,
+    }}
   >
-    <SelectLanguage />
-    <Typography level="body-xs" textAlign="center">
-      © UrbanSwisstionary {new Date().getFullYear()}
-    </Typography>
+    <Grid container spacing={{ xs: 2, md: 3 }} sx={{ flexGrow: 1 }}>
+      <Grid xs={12} sm={6} md={3}>
+        <SelectLanguage />
+      </Grid>
+      <Grid xs={12}>
+        <Typography level="body-xs" textAlign="center">
+          © UrbanSwisstionary {new Date().getFullYear()}
+        </Typography>
+      </Grid>
+    </Grid>
   </Box>
 )
 
