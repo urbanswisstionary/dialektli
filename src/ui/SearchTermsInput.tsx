d9f980ec4fb9ab7f06087789cc407758d0d332cf
odi <odi@panter.ch>
2024-02-26 13:46:45 +0100

    feat prevent the search termp input to not trigger full term query on every keystroke

--- src/ui/SearchTermsInput.tsx
+++ src/ui/SearchTermsInput.tsx
@@ -1,4 +1,4 @@
-import { useState, type FC } from "react"
+import { useState, FC } from "react"
 import Autocomplete from "@mui/joy/Autocomplete"
 import FormControl, { FormControlProps } from "@mui/joy/FormControl"
 import FormLabel from "@mui/joy/FormLabel"
@@ -6,13 +6,10 @@ import FormHelperText from "@mui/joy/FormHelperText"
 import { useRouter } from "next/router"
 import Search from "@mui/icons-material/Search"
 import { setQueryOnPage } from "@/utils/setQueryOnPage"
-import { ParsedUrlQuery } from "querystring"
 import { useQuery } from "@apollo/client"
 import { getFragmentData, graphql } from "@@/generated"
 import { TermOptionFragmentFragment } from "@@/generated/graphql"
 import { useTranslation } from "next-i18next"
-
-type Query = ParsedUrlQuery & { q: string }
 
 const TermOptionFragment = graphql(/* GraphQL */ `
   fragment TermOptionFragment on Term {
@@ -47,19 +44,16 @@ const SearchTermsInput: FC<SearchTermsInputProps> = ({
 }) => {
   const { t } = useTranslation("common", { keyPrefix: "searchTermsInput" })
   const router = useRouter()
-  const query = router.query as Query
-  const { data, previousData } = useSearchTermsQuery(
-    query.q ?? "",
-    !query.q?.length,
-  )
+  const [query, setQuery] = useState("")
+  const { data, previousData } = useSearchTermsQuery(query, !query.length)
   const [selectedOption, setSelectedOption] =
     useState<TermOptionFragmentFragment | null>(null) // store selected option to prevent a no known option error getting logged to the console
-
-  const options =
-    getFragmentData(
-      TermOptionFragment,
-      data?.termsQuery?.terms ?? previousData?.termsQuery?.terms,
-    ) ?? []
+  const options = query.length
+    ? getFragmentData(
+        TermOptionFragment,
+        data?.termsQuery?.terms ?? previousData?.termsQuery?.terms,
+      ) ?? []
+    : []
 
   return (
     <FormControl {...props} id="searchTerms" size="sm">
@@ -69,15 +63,17 @@ const SearchTermsInput: FC<SearchTermsInputProps> = ({
         options={selectedOption ? [selectedOption] : options}
         groupBy={(option) => option.title[0]?.toUpperCase()}
         getOptionLabel={(option) => option.title}
-        onChange={(_, option) => setSelectedOption(option)}
+        onChange={(_, option) => {
+          setSelectedOption(option)
+          setQueryOnPage(router, {
+            q: option?.title.length ? option?.title : null,
+          })
+        }}
         startDecorator={<Search sx={{ padding: "2px" }} />}
         blurOnSelect
         clearOnEscape
         autoComplete
-        onInputChange={(_, value) => {
-          const q = value.trim()
-          setQueryOnPage(router, { q: q.length ? q : null })
-        }}
+        onInputChange={(_, value) => setQuery(value.trim())}
       />
       {helperText ? <FormHelperText>{helperText}</FormHelperText> : null}
     </FormControl>
