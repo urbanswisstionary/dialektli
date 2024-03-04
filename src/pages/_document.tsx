766fed348571b4320d9aba3539a3bf9461cba830
odi <odi@panter.ch>
2024-03-04 22:35:54 +0100

    replace GoogleTagManager with a Google Tag Manager script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document"
 import { i18n } from "../../next-i18next.config"
-import { GoogleTagManager } from "@next/third-parties/google"
 
 const { GOOGLE_TAG_MANAGER_ID = "" } = process.env
 
@@ -16,12 +15,24 @@ export default function Document() {
           sizes="180x180"
           href="/apple-touch-icon.png"
         />
+        {GOOGLE_TAG_MANAGER_ID ? (
+          // eslint-disable-next-line @next/next/next-script-for-ga
+          <script
+            id="gtm-script"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GOOGLE_TAG_MANAGER_ID}');`,
+            }}
+          />
+        ) : null}
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
-      <GoogleTagManager gtmId={GOOGLE_TAG_MANAGER_ID} />
     </Html>
   )
 }
