5fca60b35e03b0711c00278773348dcf2600f67f
odi <odi@panter.ch>
2024-03-05 22:56:13 +0100

    Update logo color to red

--- src/ui/Logo.tsx
+++ src/ui/Logo.tsx
@@ -9,7 +9,9 @@ type LogoProps = {
  * @param {string} size - The size of the logo. Defaults to "lg".
  */
 const Logo: FC<LogoProps> = ({ size = "lg" }) => (
-  <Typography level={`title-${size}`}>Dialektli</Typography>
+  <Typography level={`title-${size}`} color="danger">
+    Dialektli
+  </Typography>
 )
 
 export default Logo
