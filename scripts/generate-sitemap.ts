9967e0e5c3c234edf1704584fa2a326ed3e61e8e
odi <odi@panter.ch>
2024-03-05 22:59:24 +0100

    Add robots.txt and sitemap.xml files, and generate-sitemap script

---
+++ scripts/generate-sitemap.ts
@@ -0,0 +1,44 @@
+/* eslint-disable no-console */
+import dayjs from "dayjs"
+import fs from "fs"
+import { globby } from "globby"
+const func = async () => {
+  console.log("Generating sitemap...")
+
+  const pages = await globby([
+    // List of pages
+    "*/pages/**/*.tsx",
+    "!*/pages/account/*.tsx",
+    "!*/pages/term/[id]/edit.tsx",
+    "!*/pages/_*.tsx",
+    "!*/pages/api", // Exclude API routes
+    "!*/pages/404.tsx", // Exclude custom 404 page
+  ])
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${pages
+  .map((page) => {
+    const path = page.replace("src/pages", "").replace(".tsx", "")
+
+    const route = path === "/index" ? "" : path
+
+    return `    <url>
+        <loc>${`https://dialektli.ch${route}`}</loc>
+        <lastmod>${dayjs().format("YYYY-MM-DD")}</lastmod>
+    </url>`
+  })
+  .join("\n")}
+</urlset>
+`
+
+  fs.writeFileSync("public/sitemap.xml", sitemap)
+  console.log("Sitemap generated!")
+}
+
+func()
+  .then(() => process.exit(0))
+  .catch((e) => {
+    console.error(e.message)
+    process.exit(-1)
+  })
