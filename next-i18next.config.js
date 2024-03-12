f33a94459edaa60df8d3f21ae87d07d0fdc441fa
odi <odi@panter.ch>
2024-03-12 16:00:04 +0100

    Add i18next types and translations for Terms of Service

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,13 @@
 const defaultLocale = /** @type {"de" | "fr" | "en"} */ ("de")
 const locales = /** @type {(typeof defaultLocale)[]} */ (["de", "fr", "en"])
 
+const defaultNS = /** @type {"common" | "tos"| "privacyPolicy"}*/ ("common")
+const namespaces = /** @type {(typeof defaultNS)[]} */ ([
+  "common",
+  "tos",
+  "privacyPolicy",
+])
+
 module.exports = {
   i18n: {
     defaultLocale,
@@ -12,6 +19,7 @@ module.exports = {
       ? require("path").resolve("./public/locales")
       : "/locales",
 
-  ns: ["common"],
+  ns: namespaces,
+  defaultNS,
   debug: process.env.NODE_ENV === "development",
 }
--- types/i18next.d.ts
+++ types/i18next.d.ts
@@ -1,13 +1,18 @@
 import "i18next"
 import type common from "../public/locales/de/common.json"
+import type tos from "../public/locales/de/tos.json"
+import type privacyPolicy from "../public/locales/de/privacyPolicy.json"
+import { defaultNS } from "../next-i18next.config"
 
 interface Resources {
   common: typeof common
+  tos: typeof tos
+  privacyPolicy: typeof privacyPolicy
 }
 
 declare module "i18next" {
   export interface CustomTypeOptions {
-    defaultNS: "common"
+    defaultNS: defaultNS
     resources: Resources
   }
 }
