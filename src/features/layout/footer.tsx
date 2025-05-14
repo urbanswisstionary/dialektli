e5fcc26e1b220fb80628c39270d08bdf6980cb19
odi <odedindi@gmail.com>
2025-05-14 14:54:36 +0200

    refactor footer component to simplify link rendering and remove unused code

--- src/features/layout/footer.tsx
+++ src/features/layout/footer.tsx
@@ -1,29 +1,27 @@
-import { useMemo, type FC } from "react"
+import type { FC } from "react"
 import Box from "@mui/joy/Box"
 import Typography from "@mui/joy/Typography"
 import SelectLocale from "@/ui/Select/selectLocale"
 import NextLink from "next/link"
-import JoyLink, { LinkProps as JoyLinkProps } from "@mui/joy/Link"
+import JoyLink from "@mui/joy/Link"
 import { useTranslation } from "next-i18next"
 import { companyName } from "@/config/constants"
 import Stack from "@mui/joy/Stack"
 import { useRouter } from "next/router"
 
+const links = [
+  { href: "/about", label: "about" },
+  { href: "/contact-us", label: "contact" },
+  { href: "/tos", label: "termsOfService" },
+  { href: "/privacy-policy", label: "privacyPolicy" },
+  { href: "/dmca", label: "dmca" },
+  { href: "/bug-report", label: "bugReport" },
+] as const
+
 const Footer: FC = () => {
   const { t } = useTranslation("common", { keyPrefix: "layout.footer" })
   const pathname = useRouter().pathname
-  console.log(pathname)
-  const links = useMemo(
-    () => [
-      { href: "/about", label: t("about") },
-      { href: "/contact-us", label: t("contact") },
-      { href: "/tos", label: t("termsOfService") },
-      { href: "/privacy-policy", label: t("privacyPolicy") },
-      { href: "/dmca", label: t("dmca") },
-      { href: "/bug-report", label: t("bugReport") },
-    ],
-    [t],
-  )
+
   return (
     <Box
       component="footer"
@@ -43,11 +41,31 @@ const Footer: FC = () => {
           alignItems="center"
           justifyContent={{ xs: "center", md: "space-between" }}
         >
-          {links.map(({ href, label }, i) => (
-            <Link key={i} href={href} active={pathname === href}>
-              {label}
-            </Link>
-          ))}
+          {links.map(({ href, label }, i) => {
+            const active = pathname === href
+            return (
+              <JoyLink
+                key={i}
+                href={href}
+                component={NextLink}
+                color={active ? "primary" : "neutral"}
+                sx={[
+                  {
+                    transition: "transform 0.1s ease",
+                    ":hover": {
+                      textDecoration: "none",
+                      transform: "scale(1.05)",
+                    },
+                  },
+                  active
+                    ? { textDecoration: "underline", pointerEvents: "none" }
+                    : {},
+                ]}
+              >
+                {t(label)}
+              </JoyLink>
+            )
+          })}
           <SelectLocale />
         </Stack>
         <Typography level="body-xs" textAlign="center">
@@ -59,28 +77,3 @@ const Footer: FC = () => {
 }
 
 export default Footer
-
-interface LinkProps extends JoyLinkProps {
-  active?: boolean
-}
-
-const Link: FC<LinkProps> = ({ sx, active, ...props }) => (
-  <JoyLink
-    component={NextLink}
-    color={active ? "primary" : "neutral"}
-    sx={[
-      {
-        transition: "transform 0.1s ease",
-        ":hover": { textDecoration: "none", transform: "scale(1.05)" },
-      },
-      active
-        ? {
-            textDecoration: "underline",
-            pointerEvents: "none",
-          }
-        : {},
-      ...(Array.isArray(sx) ? sx : [sx]),
-    ]}
-    {...props}
-  />
-)
