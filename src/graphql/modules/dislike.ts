130016680e7885ae4100f2b68fc7ff63c48c7219
odi <odedindi@gmail.com>
2026-02-25 17:34:52 +0100

    refactor: add onNull handling to Prisma object relation fields

--- src/graphql/modules/dislike.ts
+++ src/graphql/modules/dislike.ts
@@ -3,9 +3,9 @@ import { builder } from "../builder"
 builder.prismaObject("Dislike", {
   name: "Dislike",
   fields: (t) => ({
-    expression: t.relation("expression"),
+    expression: t.relation("expression", { onNull: "error" }),
     expressionId: t.exposeID("expressionId"),
-    author: t.relation("author"),
+    author: t.relation("author", { onNull: "error" }),
     authorId: t.exposeID("authorId"),
     createdAt: t.expose("createdAt", { type: "DateTime" }),
   }),
